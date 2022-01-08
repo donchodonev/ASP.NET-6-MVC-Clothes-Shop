b50af2aaadb869c5ddf72c7e15fcabdd4e7d15cb
donchodonev <dummyacc9292@gmail.com>
2022-01-08 11:57:49 +0200

    create initial project structure

---
+++ ClothesShop.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ClothesShop.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
